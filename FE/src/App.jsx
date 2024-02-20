14d16408ce1b44c3fee92a86775e7559b4c0c470
Willy Hardiantho <willyhardiantho22@gmail.com>
2024-02-20 08:33:57 +0800

    set up tailwind

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,16 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
